d692a7853c4ebf43d1ac759c5f70efa871af23c5
Volodymyr <zmiyov88@gmail.com>
2024-05-02 14:41:50 +0300

    Initial Commit

---
+++ PinterstApp/PinterstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterstAppApp.swift
+//  PinterstApp
+//
+//  Created by Vladimir Pisarenko on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct PinterstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
